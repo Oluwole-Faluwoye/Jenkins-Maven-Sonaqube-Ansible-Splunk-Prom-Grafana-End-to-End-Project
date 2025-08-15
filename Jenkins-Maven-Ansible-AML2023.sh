db2f0910baa32a658e4cf1528ab8dfbe49a07aff
unknown <faluwoyeo@yahoo.com>
2025-08-15 16:00:58 -0700

    updated project code

--- Jenkins-Maven-Ansible-AML2023.sh
+++ Jenkins-Maven-Ansible-AML2023.sh
@@ -2,26 +2,25 @@
 set -e
 
 # ----------------------------------------
-# 1. Update system
+# 1. Update the system
 # ----------------------------------------
-yum update -y
+dnf update -y
 
 # ----------------------------------------
-# 2. Install Java 17 and compiler (Amazon Corretto)
+# 2. Install Java 17 (Amazon Corretto)
 # ----------------------------------------
-amazon-linux-extras enable corretto17
-yum install -y java-17-amazon-corretto java-17-amazon-corretto-devel
+dnf install -y java-17-amazon-corretto java-17-amazon-corretto-devel
 
-# Set Java/Javac alternatives
 alternatives --install /usr/bin/java java /usr/lib/jvm/java-17-amazon-corretto/bin/java 20000
 alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-17-amazon-corretto/bin/javac 20000
 
 # ----------------------------------------
 # 3. Install Jenkins
 # ----------------------------------------
+dnf install -y wget git
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install -y jenkins git
+dnf install -y jenkins
 
 systemctl enable jenkins
 systemctl start jenkins
@@ -29,7 +28,7 @@ systemctl start jenkins
 echo "Waiting for Jenkins to start..."
 sleep 30
 echo "Jenkins initial admin password:"
-cat /var/lib/jenkins/secrets/initialAdminPassword 2>/dev/null || echo "Password file not ready yet."
+cat /var/lib/jenkins/secrets/initialAdminPassword || echo "Password not ready yet."
 
 echo "Access Jenkins at: http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4):8080"
 
@@ -51,28 +50,23 @@ chmod +x /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
 
 # ----------------------------------------
-# 5. Install Ansible + AWS plugins
-# ----------------------------------------
-amazon-linux-extras enable ansible2
-yum install -y ansible python3-pip
-pip3 install boto3
-
-# Configure Ansible defaults
-sed -i "s/^#host_key_checking = False/host_key_checking = False/" /etc/ansible/ansible.cfg
-sed -i "s/^#enable_plugins = .*/enable_plugins = aws_ec2/" /etc/ansible/ansible.cfg
-ansible-galaxy collection install amazon.aws
-
-# ----------------------------------------
-# 6. Create jenkinsmaster user
+# 5. Create jenkinsmaster user
 # ----------------------------------------
 useradd -m jenkinsmaster
 echo 'jenkinsmaster:jenkinsmaster' | chpasswd
 echo "jenkinsmaster ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
-# Give Maven ownership to jenkinsmaster
 chown -R jenkinsmaster:jenkinsmaster /opt/maven* /opt/apache-maven-${MAVEN_VERSION} /opt/maven
 echo "source /etc/profile.d/maven.sh" >> /home/jenkinsmaster/.bashrc
 echo "source /etc/profile.d/maven.sh" >> /home/jenkinsmaster/.bash_profile
+
+# ----------------------------------------
+# 6. Install Ansible + AWS plugins
+# ----------------------------------------
+dnf install -y ansible python3-pip
+pip3 install boto3
+sed -i "s/^#host_key_checking = False/host_key_checking = False/" /etc/ansible/ansible.cfg
+sed -i "s/^#enable_plugins = .*/enable_plugins = aws_ec2/" /etc/ansible/ansible.cfg
+ansible-galaxy collection install amazon.aws
 
 # ----------------------------------------
 # 7. Create ansibleadmin user
@@ -89,7 +83,7 @@ sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/ssh
 systemctl restart sshd
 
 # ----------------------------------------
-# 9. Optional: Open ports if firewalld is used
+# 9. Open Jenkins & SSH ports if firewalld is active
 # ----------------------------------------
 if systemctl is-active --quiet firewalld; then
   firewall-cmd --permanent --add-port=8080/tcp
